1d949edf62b4e86961aad88b5078e20b92afa57d
David McNeil <mcneil.david2@gmail.com>
2024-03-24 14:05:12 -0500

    Add file input

--- slotted-pig-ui/src/main.rs
+++ slotted-pig-ui/src/main.rs
@@ -1,7 +1,11 @@
+use std::sync::Arc;
+
+use anyhow::{anyhow, Result};
 use bigdecimal::BigDecimal;
 use chrono::{DateTime, Utc};
-use dioxus::prelude::*;
+use dioxus::{html::FileEngine, prelude::*};
 use log::{info, LevelFilter};
+use serde::de::DeserializeOwned;
 use slotted_pig_lib::{
     categorizer::{Categorized, CategorizedChildren, CategorizedList},
     transaction::Transaction,
@@ -26,17 +30,37 @@ fn main() {
 fn App() -> Element {
     info!("slotted-pig");
 
-    let categorized_list = use_signal(|| {
+    let mut categorized_list_result = use_signal(|| {
         let categorized = include_str!("../../personal/categorized.yaml");
-        serde_yaml::from_str::<CategorizedList>(categorized).expect("Failed to parse YAML")
+        Ok::<_, String>(
+            serde_yaml::from_str::<CategorizedList>(categorized).expect("failed to parse YAML"),
+        )
     });
 
-    let categorized_list = categorized_list().clone().categorized;
-
     rsx! {
-        div { class: "max-w-screen-lg mx-auto px-4",
-            h1 { class: "font-mono text-lg", "Slotted Pig" }
-            CategorizedList { categorized_list }
+        div { class: "max-w-screen-lg mx-auto",
+            div { class: "flex justify-between",
+                h1 { class: "font-mono text-2xl", "Slotted Pig" }
+                input {
+                    r#type: "file",
+                    accept: ".yaml",
+                    multiple: false,
+                    onchange: move |evt| {
+                        async move {
+                            *categorized_list_result
+                                .write() = read_first_file(evt.files()).await.map_err(|e| e.to_string());
+                        }
+                    }
+                }
+            }
+            match categorized_list_result.read().clone(){
+                Ok(categorized_list) =>  {
+                    rsx!(CategorizedList { categorized_list: categorized_list.categorized })
+                }
+                Err(e) => rsx!(
+                    span {"{e}"}
+                ),
+            }
         }
     }
 }
@@ -123,4 +147,17 @@ fn Amount(amount: BigDecimal) -> Element {
 fn Time(time: DateTime<Utc>) -> Element {
     let time = time.format("%Y-%m-%d");
     rsx!( span { class: "font-mono text-sm px-1", "{time}" } )
+}
+
+pub async fn read_first_file<T: DeserializeOwned>(
+    file_engine: Option<Arc<dyn FileEngine>>,
+) -> Result<T> {
+    let file_engine = file_engine.ok_or_else(|| anyhow!("missing file engine"))?;
+    let files = file_engine.files();
+    let file_name = files.first().ok_or_else(|| anyhow!("missing file"))?;
+    let contents = file_engine
+        .read_file_to_string(file_name)
+        .await
+        .ok_or_else(|| anyhow!("failed to read file as string"))?;
+    serde_yaml::from_str(&contents).map_err(|e| anyhow!("failed to deserialize file: {e}"))
 }
